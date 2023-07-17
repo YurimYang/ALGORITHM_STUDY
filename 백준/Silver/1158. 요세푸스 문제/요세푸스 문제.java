ece0977d3b6a7068b8b661a857964294e1160e80
YurimYang <63856521+YurimYang@users.noreply.github.com>
2023-07-17 17:41:04 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 376 ms, Memory: 18996 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -6,28 +6,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        int n = sc.nextInt(); // 1~n까지의 수 입력받기
+        int k = sc.nextInt(); // k번째 수 삭제하기
 
-        int index = 0;
+        int idx = 0;
 
         LinkedList<Integer> list = new LinkedList<>();
 
-        for (int i = 1; i <= n; i++) {
-            list.offer(i);
+        for (int i = 0; i < n; i++) {
+            list.add(i+1); //리스트에 1~n까지 추가하기
         }
         System.out.print("<");
         while (!list.isEmpty()) {
-            index = (index + (m - 1)) % list.size();
+            idx = (idx + (k - 1)) % list.size();
             if(list.size() != 1) {
-                System.out.print(list.remove(index) + ", ");
+                System.out.print(list.remove(idx) + ", ");
             }else{
-                System.out.print(list.remove(index));
+                System.out.print(list.remove(idx));
             }
         }
         System.out.print(">");
-
     }
 }
+
+
