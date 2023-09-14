dfc59572e871ad24911c5d81b72305e711454888
YurimYang <63856521+YurimYang@users.noreply.github.com>
2023-09-14 10:12:09 +0900

    [level 2] Title: 숫자의 표현, Time: 1.52 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int tmp = 0;
+            for (int j = i; j <= n; j++) {
+                tmp += j;
+                if (tmp == n) {
+                    answer++;
+                    break;
+                } else if(tmp > n){
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
