2721703150323a0674ae394b797acd725d226bf1
YurimYang <63856521+YurimYang@users.noreply.github.com>
2023-11-10 18:29:42 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -4,7 +4,7 @@ class Solution {
         int answer = 0;
         StringBuilder sb = new StringBuilder();
         while(n>0){
-            sb.append(n%3); //1200 
+            sb.append(n%3); //0021
             n = n/3; //15 5 1
         }
         answer = Integer.parseInt(sb.toString(),3);
