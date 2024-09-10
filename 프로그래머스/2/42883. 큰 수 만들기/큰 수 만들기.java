1673f6ac2aba080ca4aecb071ceadceab4a6a5eb
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-09-10 17:07:07 +0900

    [level 2] Title: 큰 수 만들기, Time: 7258.12 ms, Memory: 88.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String number, int k) {        
+        StringBuilder answer = new StringBuilder();
+        int start = 0;
+        int len = number.length() - k; //이 길이 만큼씩 구할 것
+        while(start < number.length() && answer.length() != len) {
+            int max = 0;
+            for(int i = start; i<k + answer.length() + 1; i++){
+                if(max < number.charAt(i) - '0'){
+                    max = number.charAt(i) - '0';
+                    start = i + 1;
+                }
+            }
+            answer.append(Integer.toString(max));
+        }
+        
+       
+        
+        return answer.toString();
+    }
+}
