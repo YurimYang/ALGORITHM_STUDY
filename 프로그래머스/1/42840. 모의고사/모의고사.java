ab1ba5430b19561695dc3bccc747cf6650eb7fd3
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-09-27 13:34:37 +0900

    [level 1] Title: 모의고사, Time: 0.94 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,50 +4,42 @@ import java.io.*;
 class Solution {
     public List<Integer> solution(int[] answers) {
         List<Integer> answer = new ArrayList<>();
+        int[] a = {1,2,3,4,5};
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+        int[] sum = new int[3];
+        
         int len = answers.length;
-        int[] newA = new int[len];
-        int[] newB = new int[len];
-        int[] newC = new int[len];
-        
-        int ansA = 0;
-        int ansB = 0;
-        int ansC = 0;
-        
-        int[] As = {1,2,3,4,5};
-        int[] Bs = {2,1,2,3,2,4,2,5};
-        int[] Cs = {3,3,1,1,2,2,4,4,5,5};
-
-
-        for(int i = 0; i<len; i++){
-            if(As[i%5] == answers[i]){
-                ansA++;
-            };
-            if(Bs[i%8] == answers[i]){
-                ansB++;
+        for(int i = 0; i< len; i++){
+            if(answers[i] == a[i % 5]){
+                sum[0]++;
+            } 
+            if(answers[i] == b[i%8]){
+                sum[1]++;
             }
-            if(Cs[i%10] == answers[i]){
-                ansC++;
+            if(answers[i] == c[i%10]){
+                sum[2]++;
             }
-        }        
+        }
         
-        int maxAnswer = 0;
-        maxAnswer = Math.max(Math.max(ansA, ansB), ansC);
+        int max = 0;
+        for(int i = 0; i<3; i++){
+           max = Math.max(max, sum[i]);
+        }
         
-        if(maxAnswer == ansA){
+        if(max == sum[0]){
             answer.add(1);
-        } 
-        if(maxAnswer == ansB){
+        }
+        if(max == sum[1]){
             answer.add(2);
         }
-        if(maxAnswer == ansC){
+        if(max == sum[2]){
             answer.add(3);
         }
+        
 
+        
+        
         return answer;
     }
 }
-
-
-//1번방식 : 1~5 나열 반복
-//2번방식 : 2 + i 번 나열 반복
-//3번방식 : 3 + 
