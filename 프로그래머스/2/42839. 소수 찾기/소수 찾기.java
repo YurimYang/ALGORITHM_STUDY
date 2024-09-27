ce72b079d8cc0b8b89da19ebc3fd9279d6e6b0f0
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-09-27 14:41:37 +0900

    [level 2] Title: 소수 찾기, Time: 57.04 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,43 +1,64 @@
 import java.util.*;
-class Solution {
-static Set<Integer> set;
-    static boolean[] visited;
-    static String[] part;
-    public static int solution(String numbers) {
-        int answer = 0;
-        set = new HashSet<>();
-        visited = new boolean[numbers.length()];
-        part = numbers.split("");
+import java.io.*;
 
-        permutation(0,"");
-        for(int num : set){
-            if(isPrimeNumber(num)){
+// 소수 판단 함수 + dfs 방식으로 찾는 함수를 구현 
+// dfs 함수를 구현할 때, String값을 Integer로 변경하여 찾을 수 잇는 값 전부를 중복없이 리스트에 담기
+// 리스트에 담긴 값들을 소수판단하여 세준다
+
+class Solution {
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static boolean[] check = new boolean[7]; //최대 7자리수
+    
+
+    public int solution(String numbers) {
+        int answer = 0;
+        
+        for(int i = 0; i<numbers.length(); i++) {
+            dfs(numbers, "", i+1);
+        }
+        
+        for(int i = 0; i<arr.size(); i++){
+            if(isPrime(arr.get(i))){
                 answer++;
             }
         }
+        
         return answer;
+        
+        
     }
-
-    public static void permutation(int idx, String sum) {
-        if(!sum.isEmpty()){
-            set.add(Integer.parseInt(sum));
-        }
-        for(int i = 0; i<part.length; i++){
-            if(!visited[i]){
-                visited[i]=true;
-                permutation(idx+1, sum + part[i]);
-                visited[i] = false;
+    
+    //dfs를 활용한 백트래킹 방법
+    public void dfs(String str, String tmp, int m) {
+        
+        if(tmp.length() == m){
+            int num = Integer.parseInt(tmp);
+            if(!arr.contains(num)){
+                arr.add(num);
             }
         }
+        
+        
+        for(int i = 0; i<str.length(); i++){
+            if(!check[i]){
+                check[i] = true;
+                tmp += str.charAt(i);
+                dfs(str,tmp, m);
+                check[i] = false;
+                tmp = tmp.substring(0, tmp.length()-1);
+            }
+        }
+        
     }
-
-
-    public static boolean isPrimeNumber(int number){
-        if(number <= 1){
+    
+    public boolean isPrime(int num) {
+        
+        if(num < 2){
             return false;
         }
-        for(int i = 2; i<=Math.sqrt(number); i++){
-            if(number % i == 0){
+        
+        for(int i = 2; i<= Math.sqrt(num); i++) {
+            if(num % i == 0) {
                 return false;
             }
         }
