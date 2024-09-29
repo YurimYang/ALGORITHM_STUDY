00a2993012f44f581bc283c6a43a87187ff0330c
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-09-29 23:26:57 +0900

    [level 2] Title: 프로세스, Time: 1.47 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,34 +1,31 @@
-import java.util.PriorityQueue;
-import java.util.Collections;
+import java.util.*;
+import java.io.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // 우선순위 큐 선언(내림 차순)
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         int answer = 0;
-
-        // 우선순위 큐에 우선순위 요소 추가
-        for (int i : priorities) {
-            queue.offer(i);
+        
+        Queue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder()); //내림차순 (default : 오름차순)
+        
+        for(int p : priorities){
+            queue.offer(p);
         }
-
-        // 큐가 빌 때까지 반복
-        while (!queue.isEmpty()) {
-            // 기존 우선순위 배열 순회
-            for (int i = 0; i < priorities.length; i++) {
-                // 현재 작업의 위치 찾기
-                if (queue.peek() == priorities[i]) {
+        
+        while(!queue.isEmpty()){
+            for(int i = 0; i< priorities.length; i++){
+                if(queue.peek() == priorities[i]) {
                     queue.poll();
                     answer++;
-
-                    // 현재 작업이 location과 같으면 answer 반환
-                    if (location == i) {
+                    
+                    if(location == i){
                         return answer;
                     }
                 }
+                
             }
         }
-
+        
+        
         return answer;
     }
 }
