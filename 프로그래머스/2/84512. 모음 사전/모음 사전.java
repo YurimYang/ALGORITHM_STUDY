c5f13884de1dbff8de6528d87c2adc53cdb75d03
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-09 22:24:36 +0900

    [level 2] Title: 모음 사전, Time: 9.71 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,24 +2,22 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    static List<String> list = new ArrayList<>();
-    static String[] dict = {"A", "E", "I", "O", "U"};
-    static int answer = 0;
+    static String[] alpha = {"A","E","I","O","U"};
+    static List<String> list= new ArrayList<>();
     public int solution(String word) {
         dfs(word, "", 0);
-        
         return list.indexOf(word);
     }
     
     public void dfs(String word, String tmp, int depth){
         list.add(tmp);
-        
         if(depth == 5){
             return;
         }
         
-        for(int i = 0; i<dict.length; i++){
-            dfs(word, tmp+dict[i], depth+1);    
+        for(int i = 0; i<5; i++){
+            dfs(word, tmp+alpha[i], depth + 1);
         }
+        
     }
 }
