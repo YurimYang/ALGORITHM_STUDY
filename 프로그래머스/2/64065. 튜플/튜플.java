0d2c7a15b7a3d6544085b894e7883ff521962ea4
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-11 19:03:01 +0900

    [level 2] Title: 튜플, Time: 58.53 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -7,32 +7,26 @@ class Solution {
         List<Integer> answer = new ArrayList<>();
         String[] split = s.substring(1,s.length()-1).split("},");
         Arrays.sort(split, (o1, o2) -> o1.length() - o2.length());
-        toCharArray("{2,1,3,4}");
         for(String str : split){
-            for(String ch : toCharArray(str)){
-                
-                    int key = Integer.parseInt(String.valueOf(ch));
-                    if(hm.containsKey(key)){
-                        continue;
-                    } else {
-                        hm.put(key, hm.getOrDefault(key,0) + 1);
-                        answer.add(key);
-                    }
-                    
-                
+            for(String ch : toStringArray(str)){
+                int key = Integer.parseInt(String.valueOf(ch));
+                if(hm.containsKey(key)){
+                    continue;
+                } else {
+                    hm.put(key, hm.getOrDefault(key,0) + 1);
+                    answer.add(key);
+                }
             }
         }
-        // {2 {2,1 {2,1,3 {2,1,3,4}
              
         return answer;
     }
     
-    public String[] toCharArray(String str){
+    public String[] toStringArray(String str){
         str = str.substring(1,str.length());
         if(str.charAt(str.length()-1) == '}'){
             str = str.substring(0,str.length()-1);
         }
         return str.split(",");
-        
     }
 }
