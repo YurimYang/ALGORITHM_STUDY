da24ca5f15757c97df17b6f568dafddb580517c5
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-11 22:40:25 +0900

    [level 3] Title: 여행경로, Time: 97.58 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,31 +2,29 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    static Queue<String> pq = new PriorityQueue<>();
-    static int[] visited;
+    static PriorityQueue<String> pq = new PriorityQueue<>();
+    static int[] used;
     public String[] solution(String[][] tickets) {
-        visited = new int[tickets.length];
+        used = new int[tickets.length];
         
         dfs("ICN", tickets, 0, "ICN");
         String[] answer = pq.peek().split("->");
         
-        
         return answer;
     }
     
-    public void dfs(String from, String[][] tickets, int depth, String path) {
+    public void dfs(String from, String[][] tickets, int depth, String path){
         if(depth == tickets.length){
             pq.offer(path);
             return;
         }
-        for(int i = 0; i< tickets.length; i++){
-            if(visited[i] == 0 && tickets[i][0].equals(from)){
-                visited[i] = 1;
+        
+        for(int i = 0; i<tickets.length; i++){
+            if(used[i] == 0 && tickets[i][0].equals(from)){
+                used[i] = 1;
                 dfs(tickets[i][1], tickets, depth + 1, path + "->" + tickets[i][1]);
-                visited[i] = 0;
+                used[i] = 0;
             }
         }
-        
     }
 }
-
