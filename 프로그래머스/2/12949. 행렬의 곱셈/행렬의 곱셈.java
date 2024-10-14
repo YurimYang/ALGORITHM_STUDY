f236881ba95390c80698c769172dac37456536fe
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-10-14 09:43:18 +0900

    [level 2] Title: 행렬의 곱셈, Time: 13.77 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -3,28 +3,13 @@ import java.io.*;
 
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[arr1.length][arr2[0].length];
-        int idx = 0;
-        //int[] reArr2 = new int[arr2.length * arr2[0].length];
-        
-//         for(int i = 0; i<arr2.length; i++){
-//             for(int j = 0; j<arr2[0].length; j++){
-//                 reArr2[idx] = reArr2[j][i];
-//                 idx++;
-//             }
-//         }
-        
-//         for(int[] arrOuter : arr1){
-//             for(int num : reArr2){
-//                 answer[]
-//             }
-//         }
-        
-        
-        
+        int width = arr2.length;
+        int length = arr2[0].length;
+        int[][] answer = new int[arr1.length][length];
+        int idx = 0; 
         for(int[] arrOuter : arr1){
-            for(int i = 0; i<arr2[0].length; i++){ 
-                for(int j = 0; j<arr2.length; j++){
+            for(int i = 0; i<length; i++){ 
+                for(int j = 0; j<width; j++){
                     answer[idx][i] += arrOuter[j] * arr2[j][i];
                 }     
             }
