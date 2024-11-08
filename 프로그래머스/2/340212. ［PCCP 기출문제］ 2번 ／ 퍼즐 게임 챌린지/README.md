f6433df10956669f96b021bfa09a7280fdafb6f3
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-11-08 15:32:03 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 38.36 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지 - 340212 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340212?language=java) 
 
 ### 성능 요약
 
-메모리: 144 MB, 시간: 48.25 ms
+메모리: 119 MB, 시간: 38.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 22일 13:28:19
+2024년 11월 08일 15:31:59
 
 ### 문제 설명
 
--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,40 +1,37 @@
 import java.util.*;
 import java.io.*;
 
-//최소의 숙련도 구하기
-//이분탐색 문제
 class Solution {
-    public int solution(int[] diffs, int[] times, long limit) {
-        return binarySearch(diffs,times,limit);
-    }
-    
-    public int binarySearch(int[] diffs, int[] times, long limit){
-        int min = 100000;
-        int max = 1;
-
-        while(max <= min){
-            long count = 0;
-            int mid = (min + max) / 2;
-            
-            for(int i = 0; i<diffs.length; i++){
-                if(mid >= diffs[i]){
-                    count += (long) times[i];
-                } else {
-                    count += (long) (times[i-1] + times[i]) * (long)(diffs[i] - mid) + (long) times[i];
-                }
+    //현재 level 값으로 가능한지 판단하는 함수
+    static boolean isImpossible(int[] diffs, int[] times, long level, long limit){
+        long t = (long)times[0];
+        
+        for(int i =1; i<times.length; i++){
+            if(diffs[i] > level){
+                t += ((long)diffs[i] - level) * ((long)times[i-1] + (long)times[i]);
             }
-            if (count > limit) {
-                max = mid + 1;   
-            } else {
-               min = mid - 1;
-            } 
+            t += (long)times[i];
         }
-        return max;
+        
+        return limit < t;
+    }
+    /*
+    이진탐색을 활용, mid값을 level로 설정
+    */
+    public int solution(int[] diffs, int[] times, long limit) {
+
+        long left = 1;
+        long right = limit;
+        
+        while(left < right){
+            long mid = (left + right) / 2;
+            
+            if(isImpossible(diffs, times, mid, limit)){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return (int)left;
     }
 }
-//diff : 난이도
-//time_cur : 퍼즐의 소요시간
-//time_prev : 이전 퍼즐의 소요시간
-//level : 내 숙련도 
-//diff-level만큼 틀림 time_cur + time_prev * diff_level + time_cur 
-
