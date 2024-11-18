1287db525e019b976ee9fe63edf62af147a3ff4a
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-11-19 08:34:32 +0900

    [level 2] Title: 피로도, Time: 3.88 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -6,20 +6,14 @@ class Solution {
     static int[] visited;
     public int solution(int k, int[][] dungeons) {
         visited = new int[dungeons.length];
-        
         dfs(dungeons, k, visited, 0);
-        
-        
-        
         return answer;
     }
     
     public void dfs(int[][] dungeons, int k, int[] visited, int depth){
-        
         if(depth > answer){
             answer = depth;
         }
-        
         for(int i = 0; i< dungeons.length; i++){
             if(visited[i] == 0 && k >= dungeons[i][0]){
                 visited[i] = 1;
