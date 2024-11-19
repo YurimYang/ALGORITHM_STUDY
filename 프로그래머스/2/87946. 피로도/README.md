2d16c48d2bfc9758064f29bf83c1ed94019748c3
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-11-19 09:11:39 +0900

    [level 2] Title: 피로도, Time: 3.11 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946#qna) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 3.88 ms
+메모리: 73.4 MB, 시간: 3.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 08:34:38
+2024년 11월 19일 09:11:45
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -3,24 +3,29 @@ import java.io.*;
 
 class Solution {
     static int answer = 0;
-    static int[] visited;
+    static int[] visited; 
     public int solution(int k, int[][] dungeons) {
         visited = new int[dungeons.length];
-        dfs(dungeons, k, visited, 0);
+        dfs(dungeons, 0, k);
+        
         return answer;
     }
-    
-    public void dfs(int[][] dungeons, int k, int[] visited, int depth){
-        if(depth > answer){
-            answer = depth;
+    public void dfs(int[][] dungeons, int result, int k){
+        if(result > answer){
+            answer = Math.max(answer,result);
+            return;
         }
-        for(int i = 0; i< dungeons.length; i++){
-            if(visited[i] == 0 && k >= dungeons[i][0]){
+            
+        for(int i = 0; i<dungeons.length; i++){
+            if(visited[i] == 0 && k - dungeons[i][0] >= 0){
                 visited[i] = 1;
-                dfs(dungeons, k-dungeons[i][1], visited, depth+1);
+                k -= dungeons[i][1];
+                result += 1;
+                dfs(dungeons, result, k);
+                result -= 1;
+                k += dungeons[i][1];
                 visited[i] = 0;
-            }
+            } 
         }
-        
     }
 }
