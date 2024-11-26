69efee330e2c801ef139a440cece1166e706d7f0
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-11-26 10:26:03 +0900

    [Silver III] Title: 파도반 수열, Time: 196 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        long[] memo = new long[101];
+
+        memo[1] = memo[2] = memo[3] = 1;
+        memo[4] = memo[5] = 2;
+        for (int i = 6; i <= 100; i++)
+            memo[i] = memo[i - 1] + memo[i - 5];
+
+        for (int tc = 1; tc <= T; tc++)
+            System.out.println(memo[sc.nextInt()]);
+
+    }
+}
