43f83fe126f6332b8619762d49d5cf5f187d51d1
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-11-26 10:29:58 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.java
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        long[] memo = new long[101];
+    public static void main(String[] args) throws IOException {
+        long[] p = new long[101];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        memo[1] = memo[2] = memo[3] = 1;
-        memo[4] = memo[5] = 2;
-        for (int i = 6; i <= 100; i++)
-            memo[i] = memo[i - 1] + memo[i - 5];
+        p[1] = p[2] = p[3] = 1;
+        p[4] = p[5] = 2;
 
-        for (int tc = 1; tc <= T; tc++)
-            System.out.println(memo[sc.nextInt()]);
+        int t = Integer.parseInt(br.readLine());
 
+        for (int i = 6; i < 101; i++) {
+            p[i] = p[i - 5] + p[i - 1];
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(p[n]);
+        }
     }
 }
