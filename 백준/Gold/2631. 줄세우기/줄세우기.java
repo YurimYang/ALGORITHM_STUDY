6ce5867554e93938ea4295deff9695d662e4c6a1
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-11-28 14:05:07 +0900

    [Gold IV] Title: 줄세우기, Time: 108 ms, Memory: 14088 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,29 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		int[] arr = new int[N];
-		int[] dp = new int[N];
-		
-		for(int i=0;i<N;i++) {
-			arr[i] = sc.nextInt();
-		} 
-		
-		int max = 0;
-		for(int i=0;i<N;i++) {
-			dp[i] = 1;
-			for(int j=0;j<i;j++) {
-				if(arr[j]<arr[i] && dp[j]+1>dp[i]) {
-					dp[i] = dp[j]+1;
-					max = Math.max(max, dp[i]);
-				}
-			}
-		}
-		
-		System.out.println(N-max);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] children = new int[n];
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < n; i++) {
+            children[i] = Integer.parseInt(br.readLine());
+        }
+
+        int maximum = 0;
+        for (int i = 0; i < n; i++) {
+            int max = dp[i];
+            for (int j = 0; j < i; j++) {
+                if (children[i] > children[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                    max = children[j];
+                }
+            }
+            maximum = Math.max(maximum, dp[i]);
+        }
+
+        System.out.println(n - maximum);
+    }
 }
