c6c1bc19a94f0059374bd5fa2b39f3a7bcff0c09
YurimYang <63856521+YurimYang@users.noreply.github.com>
2024-12-17 10:48:36 +0900

    [Gold V] Title: 용액, Time: 404 ms, Memory: 32916 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        PriorityQueue<Number> pq = new PriorityQueue<>(
+            Comparator.comparingInt(o -> Math.abs(o.res)));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int low = 0;
+        int high = n - 1;
+
+        while (low < high) {
+            int tmp = num[low] + num[high];
+            pq.offer(new Number(num[low], num[high], tmp));
+            if (tmp > 0) {
+                high -= 1;
+            } else {
+                low += 1;
+            }
+        }
+
+        System.out.println(pq.peek().num1 + " " + pq.poll().num2);
+    }
+}
+
+class Number {
+
+    int num1;
+    int num2;
+    int res;
+
+    public Number(int num1, int num2, int res) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.res = res;
+    }
+}
